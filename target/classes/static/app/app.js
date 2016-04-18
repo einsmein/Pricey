9acb4f91e696b40f5afc6bc3b56c7d5f403976d7
Mind <einsmein@hotmail.com>
2016-04-18 16:37:39 +0900

    full source

--- target/classes/static/app/app.js
+++
@@ -1,5 +0,0 @@
-(function(angular) {
-    angular.module("app.controllers", []);
-    angular.module("app.services", []);
-    angular.module("app", ["ngResource", "app.controllers", "app.services"]);
-}(angular));
